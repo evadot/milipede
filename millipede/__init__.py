9042142b11000dc9f22ceb7d5ee7b8d968eef304
Edouard Bonlieu <ebonlieu@ocs.online.net>
2015-07-24 16:45:38 +0200

    Added templates to customize your milipede

--- millipede/__init__.py
+++ millipede/__init__.py
@@ -10,17 +10,34 @@ from argparse import ArgumentParser
 import sys
 
 
-def millipede(size, comment=None, reverse=False):
+def millipede(size, comment=None, reverse=False, template='default'):
     """
     Output the millipede
     """
     padding_offsets = [2, 1, 0, 1, 2, 3, 4, 4, 3]
 
+    templates = {
+            'frozen': {'body': '╔═(❄❄❄)═╗', 'bodyr': '╚═(❄❄❄)═╝'},
+            'love': {'body': '╔═(♥♥♥)═╗', 'bodyr': '╚═(♥♥♥)═╝'},
+            'corporate': {'body': '╔═(©©©)═╗', 'bodyr': '╚═(©©©)═╝'},
+            'musician': {'body': '╔═(♫♩♬)═╗', 'bodyr': '╚═(♫♩♬)═╝'},
+            'bocal': {'body': '╔═(🐟🐟🐟)═╗', 'bodyr': '╚═(🐟🐟🐟)═╝'},
+            'default': {'body': '╔═(███)═╗', 'bodyr': '╚═(███)═╝'},
+        }
+
+    print(template)
+    if template not in templates:
+        template = templates.get('default')
+    else:
+        print(templates[template])
+        template = templates.get(template)
+
+
     head = "    ╔⊙ ⊙╗\n" if reverse else "    ╚⊙ ⊙╝\n"
     body = "".join([
         "{}{}\n".format(
             " " * padding_offsets[(x + 3) % 9 if reverse else x % 9],
-            "╔═(███)═╗" if reverse else "╚═(███)═╝"
+            template['body'] if reverse else template['bodyr']
         )
         for x in range(size)
     ])
@@ -51,8 +68,11 @@ def main():
     parser.add_argument('-r', '--reverse',
                         action='store_true',
                         help='reverse the millipede')
+    parser.add_argument('-t', '--template',
+                        help='customize your millipede')
+
     args = parser.parse_args()
 
     sys.stdout.write(
-        millipede(args.size, comment=args.comment, reverse=args.reverse)
+        millipede(args.size, comment=args.comment, reverse=args.reverse, template=args.template)
     )
